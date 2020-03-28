b9deb6a2c3da1a005d2af4c42fcc29ee65e030ed
yannikmaassen <yannik.maassen@t-online.de>
2020-03-29 00:47:36 +0100

    Comitting before deploy with now

--- docs/main.fb6bbcaf.js
+++ docs/main.fb6bbcaf.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56889" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51044" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
